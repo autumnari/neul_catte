7fce170aca299f121e0e75aabc05758146c42644
yeongjo <playinlion@likelion.org>
2018-08-13 10:09:49 +0900

    구글 애널리틱스 추가

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180810080030) do
+ActiveRecord::Schema.define(version: 20180810091159) do
+
+  create_table "musics", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
