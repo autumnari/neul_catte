36daed7f9c3d5670b75f3ac46fe86921647fb191
Youngjin Kwon <kyjinamjsu1@naver.com>
2018-08-24 22:05:37 +0900

    자동완성기능
    tape에 노래 입력 시 자동 완성

--- Gemfile
+++ Gemfile
@@ -23,6 +23,12 @@ gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+# Use jquery-ui
+gem 'jquery-ui-rails'
+
+# Use jquery as the JavaScript library
+gem 'jquery-rails'
+
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- app/controllers/tapes_controller.rb
+++ app/controllers/tapes_controller.rb
@@ -5,23 +5,42 @@ class TapesController < ApplicationController
 
   def new
     @tape = Tape.new
+
+    @musics = Music.all
+
+    @music_tscollection = Array.new()
+    @musics.each do |music|
+      @music_tscollection.push music.ts
+    end
   end
 
   def create
+    
+    @correct_music1 = Music.find_by_ts(params[:tape][:m1])
+    @correct_music2 = Music.find_by_ts(params[:tape][:m2])
+    @correct_music3 = Music.find_by_ts(params[:tape][:m3])
+    @correct_music4 = Music.find_by_ts(params[:tape][:m4])
+    @correct_music5 = Music.find_by_ts(params[:tape][:m5])
+    @correct_music6 = Music.find_by_ts(params[:tape][:m6])
+    @correct_music7 = Music.find_by_ts(params[:tape][:m7])
+    @correct_music8 = Music.find_by_ts(params[:tape][:m8])
+    @correct_music9 = Music.find_by_ts(params[:tape][:m9])
+    @correct_music10 = Music.find_by_ts(params[:tape][:m10])
+
     tape = Tape.new
 
     tape.tape_title = params[:tape][:tape_title]
     tape.user_id = current_user.id
-    tape.m1 = params[:tape][:m1]
-    tape.m2 = params[:tape][:m2]
-    tape.m3 = params[:tape][:m3]
-    tape.m4 = params[:tape][:m4]
-    tape.m5 = params[:tape][:m5]
-    tape.m6 = params[:tape][:m6]
-    tape.m7 = params[:tape][:m7]
-    tape.m8 = params[:tape][:m8]
-    tape.m9 = params[:tape][:m9]
-    tape.m10 = params[:tape][:m10]
+    tape.m1 = @correct_music1.id
+    tape.m2 = @correct_music2.id
+    tape.m3 = @correct_music3.id
+    tape.m4 = @correct_music4.id
+    tape.m5 = @correct_music5.id
+    tape.m6 = @correct_music6.id
+    tape.m7 = @correct_music7.id
+    tape.m8 = @correct_music8.id
+    tape.m9 = @correct_music9.id
+    tape.m10 = @correct_music10.id
     tape.save
 
     redirect_to new_tape_path
