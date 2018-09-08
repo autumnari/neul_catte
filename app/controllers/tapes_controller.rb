feab1f5a1a09c1ee84d3e2ef03436339e1314869
이원섭wonsup Lee <wonsup@team-milestone.com>
2018-09-08 17:53:05 +0900

    request: 숟가락 얹기  (#95)
    * 테이프의 트랙을 다 채우지 않았을 때 생기는 에러 수정
    
    correct_music# 오브젝트에 id 를 가져오는 것을 시도(try) 하고 없으면 nil 반환
    
    * 입력이 잘 됐는지 확인 용도 겸 수정 용도로 edit 생성
    
    * 테이프 수정 화면에서 기록된 노래들의 ts 가 나올 수 있게 변경
    
    * show 대신에  edit 페이지 보여주기
    
    show를 따로 만들기 귀찮으니까 edit 로 대체해봤습니다.
    
    * 수정 링크 추가

--- app/controllers/tapes_controller.rb
+++ app/controllers/tapes_controller.rb
@@ -5,46 +5,61 @@ class TapesController < ApplicationController
 
   def new
     @tape = Tape.new
-
     @musics = Music.all
+    @music_tscollection = Music.pluck(:ts)
+  end
 
-    @music_tscollection = Array.new()
-    @musics.each do |music|
-      @music_tscollection.push music.ts
+  def edit
+    @tape = Tape.find(params[:id])
+    @musics = Music.all
+    @music_tscollection = Music.pluck(:ts)
+  end
+
+  def update
+    tape = Tape.find(params[:id])
+    (1..10).to_a.each do |i|
+      current_music = "m#{i}".to_sym
+      music = nil
+      if tape_params[current_music].present?
+        music = Music.find_by_ts(tape_params[current_music][:ts])
+      end
+      tape_params[current_music] = music
     end
+    tape.update!(tape_params)
+    # tape.save!
+    redirect_to edit_tape_path(tape), notice: "수정되었습니다."
   end
 
   def create
-
-    @correct_music1 = Music.find_by_ts(params[:tape][:m1])
-    @correct_music2 = Music.find_by_ts(params[:tape][:m2])
-    @correct_music3 = Music.find_by_ts(params[:tape][:m3])
-    @correct_music4 = Music.find_by_ts(params[:tape][:m4])
-    @correct_music5 = Music.find_by_ts(params[:tape][:m5])
-    @correct_music6 = Music.find_by_ts(params[:tape][:m6])
-    @correct_music7 = Music.find_by_ts(params[:tape][:m7])
-    @correct_music8 = Music.find_by_ts(params[:tape][:m8])
-    @correct_music9 = Music.find_by_ts(params[:tape][:m9])
-    @correct_music10 = Music.find_by_ts(params[:tape][:m10])
+    @correct_music1 = Music.find_by_ts(params[:tape][:m1][:ts])
+    @correct_music2 = Music.find_by_ts(params[:tape][:m2][:ts])
+    @correct_music3 = Music.find_by_ts(params[:tape][:m3][:ts])
+    @correct_music4 = Music.find_by_ts(params[:tape][:m4][:ts])
+    @correct_music5 = Music.find_by_ts(params[:tape][:m5][:ts])
+    @correct_music6 = Music.find_by_ts(params[:tape][:m6][:ts])
+    @correct_music7 = Music.find_by_ts(params[:tape][:m7][:ts])
+    @correct_music8 = Music.find_by_ts(params[:tape][:m8][:ts])
+    @correct_music9 = Music.find_by_ts(params[:tape][:m9][:ts])
+    @correct_music10 = Music.find_by_ts(params[:tape][:m10][:ts])
 
     tape = Tape.new
 
     tape.tape_title = params[:tape][:tape_title]
     tape.user_id = current_user.id
-    tape.m1 = @correct_music1.id
-    tape.m2 = @correct_music2.id
-    tape.m3 = @correct_music3.id
-    tape.m4 = @correct_music4.id
-    tape.m5 = @correct_music5.id
-    tape.m6 = @correct_music6.id
-    tape.m7 = @correct_music7.id
-    tape.m8 = @correct_music8.id
-    tape.m9 = @correct_music9.id
-    tape.m10 = @correct_music10.id
+    tape.m1 = @correct_music1 || nil
+    tape.m2 = @correct_music2 || nil
+    tape.m3 = @correct_music3 || nil
+    tape.m4 = @correct_music4 || nil
+    tape.m5 = @correct_music5 || nil
+    tape.m6 = @correct_music6 || nil
+    tape.m7 = @correct_music7 || nil
+    tape.m8 = @correct_music8 || nil
+    tape.m9 = @correct_music9 || nil
+    tape.m10 = @correct_music10 || nil
     tape.save
-
-    redirect_to new_tape_path
+    redirect_to edit_tape_path(tape), notice: "테이프에 노래가 들어갔습니다."
   end
+
 
   def show
     @tape = Tape.find_by(id: params[:id])
@@ -62,5 +77,25 @@ class TapesController < ApplicationController
     song << @tape.m10
 
     @list = Music.where(id: song)
+  end
+
+  private
+
+  def found_music(param)
+    result = nil
+    if param.is_a?(String)
+      result = Music.find_by_ts(params[:tape][:m1])
+    elsif param.is_a?(Music)
+      result = Music.find_by_ts(params[:tape][:m1])
+    end
+    result
+  end
+
+  def set_data
+    @tape = find_by.params[:tape_id]
+  end
+
+  def tape_params
+    params.require(:tape).permit!
   end
 end
--- app/models/tape.rb
+++ app/models/tape.rb
@@ -1,4 +1,10 @@
 class Tape < ApplicationRecord
   belongs_to :user
+  belongs_to :m1, class_name: 'Music', foreign_key: 'm1', optional: true
+  belongs_to :m2, class_name: 'Music', foreign_key: 'm2', optional: true
+  belongs_to :m3, class_name: 'Music', foreign_key: 'm3', optional: true
+  (4..10).to_a.each do |i|
+    belongs_to "m#{i}".to_sym, class_name: 'Music', foreign_key: "m#{i}", optional: true
+  end
   has_many :musics, :as => :fantastic
 end
