e065051f8ecb06cc548056eb7cbc23dc471ba1d6
Youngjin Kwon <kyjinamjsu1@naver.com>
2018-09-08 17:40:48 +0900

    인생 10곡 자동완성
    코드 문제가 아닌 것 같은데 이상하게 안되네요.

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,14 @@
 class RecordsController < ApplicationController
   def new
     @list = Record.new
+
+
+    @musics = Music.all
+
+    @music_tscollection = Array.new()
+    @musics.each do |music|
+      @music_tscollection.push music.ts
+    end
     # @list.user_id = current_user.id
     # @list.music_id = params[:record][:music_id]
     # @list.save
